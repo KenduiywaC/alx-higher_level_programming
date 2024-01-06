0a7679dba92285930483111e4bc55587be227a86
KenduiywaC <kendiefrk@gmail.com>
2024-01-06 13:57:30 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built for 0x08. Python - More Classes and Objects task 0.
+"""
+
+
+class Rectangle:
+    """Empty class.
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+**This is my README.md file for 0x08. Python - More Classes and Object Python Project**
