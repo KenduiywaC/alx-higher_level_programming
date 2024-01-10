2e4530ce53224674e5f4352c400b1ad7832bd1c0
KenduiywaC <kendiefrk@gmail.com>
2024-01-10 08:50:06 -0800

    0-main.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+"""Defines addition of integers."""
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Cast to integers if they are floats
+    a = int(a)
+    b = int(b)
+
+    return a + b
