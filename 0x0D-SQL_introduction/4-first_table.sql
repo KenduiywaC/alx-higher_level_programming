431238f51dc60c2e5e83416f11518102b512f94b
KenduiywaC <kendiefrk@gmail.com>
2024-03-16 07:33:21 -0700

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table in the database.
+-- Excecute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0.
+CREATE TABLE IF NOT EXISTS ${hbtn_0c_0}.first_table (id INT, name VARCHAR(256));
