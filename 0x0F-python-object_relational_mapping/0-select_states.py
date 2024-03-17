1dd8543dcb4c69071b4937bb9936fcf7e40d7236
KenduiywaC <kendiefrk@gmail.com>
2024-03-17 13:07:32 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-        cursor.close()
+    [print(state) for state in cursor.fetchall()]
