6b54fa2773b95efeb492d7b50ba98ab9d112071e
KenduiywaC <kendiefrk@gmail.com>
2024-04-24 07:21:15 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (error, data) 
+	{
+		if (error) 
+		{
+			console.log('Error from reading the file:', error);
+
+			} else {
+				process.stdout.write(data);
+  }
+		});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+**This is my README.md file for the 0x14. JavaScript - Web scraping project**
