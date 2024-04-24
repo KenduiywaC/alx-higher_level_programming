5b167006730f56be32ed99ebd52bde5a0643ee95
KenduiywaC <kendiefrk@gmail.com>
2024-04-24 07:27:15 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,12 +3,12 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (error, data) 
-	{
-		if (error) 
-		{
-			console.log('Error from reading the file:', error);
+{
+if (error) 
+{
+console.log('Error from reading the file:', error);
 
-			} else {
-				process.stdout.write(data);
-  }
-		});
+} else {
+process.stdout.write(data);
+}
+});
